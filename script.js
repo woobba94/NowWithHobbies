c01bea41d7f7ed76d9c13daff56ca00fedfc87fb
woobba94 <jwj3199@naver.com>
2021-11-29 23:54:52 +0900

    update__result-page-rolling

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// 가중치 저장 변수 선언
+// 실내선호 가중치
+var home_count = 0;
+// 이과성향 가중치
+var it_count = 0;
+// 외향성 가중치
+var extrovert_count = 0;
+// 남성성 가중치
+var male_count = 0;
+
 var radios = document.querySelectorAll("input");
 for (var i = 0, max = radios.length; i < max; i++) {
     radios[i].onclick = function () {
@@ -24,16 +34,6 @@ for (var i = 0, max = radios.length; i < max; i++) {
 }
 
 function result() {
-    // 가중치 저장 변수 선언
-    // 실내선호 가중치
-    var home_count = 0;
-    // 이과성향 가중치
-    var it_count = 0;
-    // 외향성 가중치
-    var extrovert_count = 0;
-    // 남성성 가중치
-    var male_count = 0;
-
     // 케이스 수만큼 반복
     for (var t = 0; t < 4; t++) {
         var case_name,
@@ -62,6 +62,8 @@ function result() {
         else male_count = count;
         console.log("home_count : " + home_count);
     }
+    testDataSave();
+    // showResultPage();
 
     // // 대답안한 문항이 있는지 확인하기
     // // no라고 대답한 갯수까지 합쳐서 5이면 모든 질문에 대답했다는 의미
@@ -90,7 +92,103 @@ function result() {
     // else if (yes_counter == 1)
     //     location.href = "result_2.html";
     // else location.href = "result_1.html";
+}
+function testDataSave() {
+    sessionStorage.setItem("home_count", home_count);
+    sessionStorage.setItem("it_count", it_count);
+    sessionStorage.setItem(
+        "extrovert_count",
+        extrovert_count
+    );
+    sessionStorage.setItem("male_count", male_count);
+}
 
+let ul = document.getElementById("list");
+let len = 0;
+
+function initResultPage() {
+    let home = dataLoad("home_count");
+    let it = dataLoad("it_count");
+    let extrovert = dataLoad("extrovert_count");
+    let male = dataLoad("male_count");
+    let pattern = home + it + extrovert + male;
+    let hobby = "";
+    if (pattern == "0000") {
+        hobby = "리그오브레전드";
+    } else if (pattern == "0001") {
+        hobby = "이런취미";
+    } else if (pattern == "0010") {
+        hobby = "저런취미";
+    } else if (pattern == "0011") {
+        hobby = "요론취미";
+    } else if (pattern == "0100") {
+        hobby = "짱구보기";
+    } else if (pattern == "0101") {
+        hobby = "숨쉬기";
+    } else if (pattern == "0110") {
+        hobby = "널뛰기";
+    } else if (pattern == "0111") {
+        hobby = "asda";
+    } else if (pattern == "1000") {
+        hobby = "asda";
+    } else if (pattern == "1001") {
+        hobby = "asda";
+    } else if (pattern == "1010") {
+        hobby = "asda";
+    } else if (pattern == "1011") {
+        hobby = "asda";
+    } else if (pattern == "1100") {
+        hobby = "asda";
+    } else if (pattern == "1101") {
+        hobby = "레고 조립";
+    } else if (pattern == "1110") {
+        hobby = "asda";
+    } else {
+        hobby = "asda";
+    }
+    setRollingData(hobby);
+    moveContainer();
+    sessionStorage.clear();
+}
+
+function dataLoad(key) {
+    let result = "0";
+    if (sessionStorage.getItem(key) > 0) {
+        result = "1";
+    }
+    return result;
+}
+
+function setRollingData(hobby) {
+    let hobbylist = [
+        "이런취미",
+        "저런취미",
+        "요론취미",
+        "음..",
+    ];
+    hobbylist.push(hobby);
+    len = hobbylist.length;
+    for (let j = 0; j < 5; j++) {
+        for (let i = 0; i < len; i++) {
+            var li = document.createElement("li");
+            li.appendChild(
+                document.createTextNode(hobbylist[i])
+            );
+            ul.appendChild(li);
+        }
+    }
+}
+
+function moveContainer() {
+    let choosenOption = ul.children[len * 3 - 1];
+    setTop(-choosenOption.offsetTop);
+}
+
+function setTop(top) {
+    container.style.top = `${top}px`;
+}
+
+function showResultPage() {
     // Counter에 따라 다른 결과 페이지로 이동하기
     if (home_count > 0) {
         if (it_count > 0) {
